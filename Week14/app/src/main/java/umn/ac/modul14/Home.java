40c4d0ca3b055cd1168ae85d34b752ac7c061c88
Darmadi <49474854+DarmadiC@users.noreply.github.com>
2020-05-05 22:01:20 +0700

    Add files via upload

---
+++ Week14/app/src/main/java/umn/ac/modul14/Home.java
@@ -0,0 +1,14 @@
+package umn.ac.modul14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ Week14/app/src/main/java/umn/ac/modul14/Tambah.java
@@ -0,0 +1,14 @@
+package umn.ac.modul14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tambah extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah);
+    }
+}
