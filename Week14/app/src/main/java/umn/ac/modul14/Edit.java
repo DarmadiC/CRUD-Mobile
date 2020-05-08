35796b198b37b3e3b5b4718a6ca58355cac72244
Darmadi <darmadic199@gmail.com>
2020-05-08 21:53:51 +0700

    Alert Button

--- Week14/app/src/main/java/umn/ac/modul14/Edit.java
+++ Week14/app/src/main/java/umn/ac/modul14/Edit.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,7 +17,7 @@ import java.util.ArrayList;
 
 public class Edit extends AppCompatActivity {
 
-
+    private Button btnHapus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,13 @@ public class Edit extends AppCompatActivity {
         setContentView(R.layout.activity_edit);
         Button btnKembali = (Button)findViewById(R.id.btnKembali);
         Button btnEdit = (Button)findViewById(R.id.btnEdit);
+        Button btnHapus = (Button)findViewById(R.id.btnHapus);
+        btnHapus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
 
         btnKembali.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,7 +41,35 @@ public class Edit extends AppCompatActivity {
             }
         });
     }
+    private void showDialog(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                this);
 
+        // set title dialog
+        alertDialogBuilder.setTitle("Konfirmasi hapus data ini?");
 
+        // set pesan dari dialog
+        alertDialogBuilder
+                .setIcon(R.mipmap.ic_launcher)
+                .setCancelable(false)
+                .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        // jika tombol diklik, maka akan menutup activity ini
+                        Edit.this.finish();
+                    }
+                })
+                .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // jika tombol ini diklik, akan menutup dialog
+                        // dan tidak terjadi apa2
+                        dialog.cancel();
+                    }
+                });
 
-}
+        // membuat alert dialog dari builder
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // menampilkan alert dialog
+        alertDialog.show();
+    }
+    }
